e1f4a651beae583a5e13843df9b1a1944a5bb3d0
Suresh Gururajan <sgr@asu.edu>
2016-03-05 09:04:48 -0700

    Assignment 2 updated

---
+++ Assignment_2/app/build/generated/source/buildConfig/debug/com/mcassignments/a2/cse535_assignment2/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mcassignments.a2.cse535_assignment2;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.asu.impact.cse535_example1";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
